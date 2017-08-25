87d9f7cd9833e95816a1ee9574261afb9d0cec79
inyounghong <ih235@cornell.edu>
2017-08-24 23:59:16 -0400

    Grid design

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -3,20 +3,22 @@ import AboutSection from './about/about';
 import Home from './home/home';
 import EducationSection from './home/education/education';
 import ExperienceSection from './home/experience/experience';
-import ProjectsSection from './home/projects/projects';
+import Project from './home/project/project';
 import TopSection from './home/top/top';
 import Projects from './projects/projects';
 import Contact from './contact/contact';
+import Sidebar from './sidebar/sidebar';
 
 let componentModule = angular.module('app.components', [
     Home,
     AboutSection,
     EducationSection,
     ExperienceSection,
-    ProjectsSection,
+    Project,
     TopSection,
     Projects,
     Contact,
+    Sidebar,
 ])
 
 .name;
