ea2909492bc7ccd4fdc91932306c1540fd134ef6
inyounghong <ih235@cornell.edu>
2017-08-28 14:39:31 -0400

    Add new project pages

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -2,7 +2,10 @@ import template from './app.html';
 import './app.scss';
 
 let appComponent = {
-  template
+  template,
+  controller: function() {
+      this.loaded = true;
+  }
 };
 
 export default appComponent;
--- client/app/components/detail/detail.controller.js
+++ client/app/components/detail/detail.controller.js
@@ -2,11 +2,15 @@ import projects from './../../data/projects.json';
 
 class DetailController {
     constructor($stateParams, $location) {
-        this.name = 'detail';
-        this.projectId = $stateParams.projectId;
-        this.imagePath = $location.absUrl().split('/').slice(0, 3).join("/") + "/images/" + $stateParams.projectId + "/";
 
-        console.log(projects[this.projectId]);
+        var id = $stateParams.projectId;
+
+        this.name = 'detail';
+        this.projectId = id;
+        this.project = projects[id];
+        this.imagePath = "./images/" + id;
+        this.includePath = '../../../pages/' + id + '.html';
+
     }
 }
 DetailController.$inject = ["$stateParams", "$location"];
